2889e56a796a73d4d40e82492fb5b4ce37dcb18a
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-11-01 08:06:01 +0300

    Edit console example output

--- src/RoadTransport.java
+++ src/RoadTransport.java
@@ -27,28 +27,29 @@ class RoadTransport {
     }
 
     public void print() {
-        System.out.println("Port Of Loading: " + journey.getPortOfLoading());
-        System.out.println("Port Of Discharge: " + journey.getPortOfDischarge());
-        System.out.println("Departure DateTime: " + journey.getDepartureDateTime());
-        System.out.println("Arrival DateTime: " + journey.getArrivalDateTime());
-        System.out.println("Distance: " + journey.getDistanceKm() + "km");
+        System.out.println("Journey:");
+        System.out.println(" Port Of Loading: " + journey.getPortOfLoading());
+        System.out.println(" Port Of Discharge: " + journey.getPortOfDischarge());
+        System.out.println(" Departure DateTime: " + journey.getDepartureDateTime());
+        System.out.println(" Arrival DateTime: " + journey.getArrivalDateTime());
+        System.out.println(" Distance (km): " + journey.getDistanceKm());
 
         System.out.println();
+        System.out.println("Drivers:");
 
         for (Driver driver : drivers) {
-            System.out.println("Driver Name: " + driver.getName());
-            System.out.println("Driver Lastname: " + driver.getLastname());
-            System.out.println("Driver Identity Number: " + driver.getIdentityNumber());
-            System.out.println("Driver License Number: " + driver.getLicenseNumber());
-            System.out.println("---");
+            System.out.println("- Name: " + driver.getName());
+            System.out.println("  Lastname: " + driver.getLastname());
+            System.out.println("  Identity Number: " + driver.getIdentityNumber());
+            System.out.println("  License Number: " + driver.getLicenseNumber());
         }
 
         System.out.println();
+        System.out.println("Cargos:");
 
         for (Cargo cargo : cargos) {
-            System.out.println("Cargo Description: " + cargo.getDescription());
-            System.out.println("Cargo Weight (kg): " + cargo.getWeight());
-            System.out.println("---");
+            System.out.println("- Description: " + cargo.getDescription());
+            System.out.println("  Weight (kg): " + cargo.getWeight());
         }
     }
 }
